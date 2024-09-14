00c39caba76602bed577aa36ecc0ad891d3136ab
Sgedelta <Sgeaston04@gmail.com>
2024-09-13 22:01:34 -0400

    finishing work on streaming media hw

--- src/htmlResponses.js
+++ src/htmlResponses.js
@@ -8,3 +8,21 @@ const getIndex = (request, response) => {
   response.end();
 };
 module.exports.getIndex = getIndex;
+
+const pageTwo = fs.readFileSync(`${__dirname}/../client/client2.html`);
+
+const getPageTwo = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(pageTwo);
+  response.end();
+};
+module.exports.getPageTwo = getPageTwo;
+
+const pageThree = fs.readFileSync(`${__dirname}/../client/client3.html`);
+
+const getPageThree = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(pageThree);
+  response.end();
+};
+module.exports.getPageThree = getPageThree;
